bf0a73937136f83884b5ee654fa0489771f6b1ab
yichuan <yichuan@mogu.com>
2020-11-04 12:28:09 +0800

    add spring-demo module

---
+++ spring-demo/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import service.MyService;
+
+/**
+ * @Author: yichuan
+ * @Date: 2020/11/4 10:59 上午
+ * @Description:
+ */
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+		MyService myService = applicationContext.getBean("myService", MyService.class);
+		myService.hi();
+
+	}
+}
---
+++ spring-demo/src/main/java/service/MyService.java
@@ -0,0 +1,12 @@
+package service;
+
+/**
+ * @Author: yichuan
+ * @Date: 2020/11/4 10:58 上午
+ * @Description:
+ */
+public class MyService {
+	public void hi() {
+		System.out.println("Hello Spring");
+	}
+}
